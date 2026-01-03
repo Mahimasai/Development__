4a24c405dad434624bf3ed876147aaf95caa3b44
Mahimasai <saimahimanvitha2006@gmail.com>
2026-01-03 10:27:36 +0530

    Comparision

---
+++ JavaScript_Hands_On/01_Basics/04_Comparision.js
@@ -0,0 +1,20 @@
+//Here we know about the normal Comparators
+
+// console.log("2" > 1);
+// console.log("02 " > 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+//Here comparision operators works differently when compared to equaltiy check
+
+//comparision operators >,<,>=,<= convert the null to number and then compare   so null is converted to 0
+
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+
+// === checks the value and the ttype
+
+// console.log("2" === 2);//false
