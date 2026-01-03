4a74408f844f92226501a7c6ae638874ff024e2e
Mahimasai <saimahimanvitha2006@gmail.com>
2026-01-03 08:45:44 +0530

    Intro to Java Script

---
+++ JavaScript_Hands_On/01_Basics/01_Variables.js
@@ -0,0 +1,37 @@
+const accountId = 144986565
+let accountEmail = "saima@123"
+var accountPassword = "123456"
+accountCity = "Hyderabad"
+
+let accountState; //Declaration
+
+
+// accountId = 2 //Not allowed to change const
+
+console.log(accountId)
+
+accountEmail = "saima@123.hccc.com"
+accountPassword = "5454634756"
+accountCity = "Bangalore"
+
+// console.log(accountEmail)
+// console.log(accountPassword)
+// console.log(accountCity)
+//Instead of above 3 lines we can use conssole.table
+
+//Here the accountState is undefined because we have only declared it but not initialized it
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+//But you saw that let and var are almost same
+//Earlier JavaScript only had var keyword to declare variables
+//It created a huge problem in scope where the scope problem was not defined
+//So to resolve this issue, let and const keywords were introduced
+//There is only one difference between the let and var that is scope
+//Scope of var is function scope
+//Scope of let is block scope
+
+/*
+Prefer not to use var because of issue in block scope and functional scope 
+
+*/
+
---
+++ JavaScript_Hands_On/01_Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using node, not browser
+
+//Code Readability really
+
+console.log(3 + 3); //Addition
+
+//ecma script or es documents the JS standards
+
+//https://tc39.es/ecma262/  --Ecma Script
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript   Web Docs
+
