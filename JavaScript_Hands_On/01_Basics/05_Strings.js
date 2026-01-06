c925807badd717488c1550ca658814b697734a82
Mahimasai <saimahimanvitha2006@gmail.com>
2026-01-06 13:25:49 +0530

    String Methods

--- JavaScript_Hands_On/01_Basics/05_Strings.js
+++ JavaScript_Hands_On/01_Basics/05_Strings.js
@@ -11,7 +11,7 @@ console.log(gameName.toLowerCase());
 
 console.log(gameName.charAt(2));
 
-console.log(gameName.indexof("t"));
+console.log(gameName.indexOf("t"));
 
 newString = gameName.substring(0, 4); //Last not included negative values are not allowed
 
@@ -25,3 +25,19 @@ const newString1 = "     sai mahima      "; // here we have extra unnecessary ch
 
 console.log(newString1);
 console.log(newString1.trim()); // trim function removes extra spaces from starting and ending
+
+const url = "https://saima.com/mahima%20saima"; //here spaces are %20
+
+console.log(url.replace("%20", "-"));
+
+url.includes("saima"); //Does this string have this substring
+
+url.includes("maho");
+
+//I wanna conver the string into arrays based on the certain parameters
+
+console.log(url.split("/")); // It outputs the array as ["https:", "saima.com", "mahima%20saima"]
+
+//we wll use methods majorly in frontend and backend
+
+//There are many methods if we wanna knoow more then go to console and then type any string and enter then type . and enter then you will see all the methods
