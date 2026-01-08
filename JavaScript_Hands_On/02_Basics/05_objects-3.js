94134c62dd155dc34f9711e1db27d18b7693243b
Mahimasai <saimahimanvitha2006@gmail.com>
2026-01-09 01:21:51 +0530

    changes

--- JavaScript_Hands_On/02_Basics/05_objects-3.js
+++ JavaScript_Hands_On/02_Basics/05_objects-3.js
@@ -10,4 +10,21 @@ const course = {
 
 const { courseInstructor } = course;
 
-console.log(courseInstructor);
+const { courseInstructor: instructor } = course;
+
+// console.log(courseInstructor);
+// console.log(instructor);// Here instead of the full Name we can use the alias
+
+//wE TALK ABOUT THE API
+//API is the application programming interface
+
+//Like we ask services through the API
+//Like menu card we order services we get the values in the xml previously now we get the values mostly in json
+
+// {
+//     name:"saime",
+//     courseName:"js in hindi by hitesh sir",
+//     price:"999"
+// }//Without the variable it will be treated as json
+
+//Sometimes we might also get the values in the array
