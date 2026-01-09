06cd13f1e414bd982dcaacc7e5d6b102bb8562d5
Mahimasai <saimahimanvitha2006@gmail.com>
2026-01-09 12:37:31 +0530

    functions

---
+++ JavaScript_Hands_On/03_Basics/02_functions.js
@@ -0,0 +1,35 @@
+//When you don't know the number of arguments
+
+function calculateCartPrice(...num3) {
+  return num3;
+}
+console.log(calculateCartPrice(200, 400, 500, 2000)); //here we get array
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+console.log(calculateCartPrice(200, 400, 500, 2000)); //returns [500,2000 ]
+
+const user = {
+  username: "hitesh",
+  price: 199,
+};
+function handleObject(anyobject) {
+  console.log(
+    `Usrname is ${anyobject.username} and price is ${anyobject.price} `
+  );
+}
+//The type creates a problem here
+// handleObject(user);
+handleObject({
+  username: "saam",
+  price: 199,
+});
+
+const myNewArray = [200, 400, 500, 2000];
+function returnSecondValue(getarry) {
+  return getarry[1];
+}
+// console.log(returnSecondValue(myNewArray));
+
+//function scope
