761a618d10bd56d02f752a0b710943889f0507a4
Mahimasai <saimahimanvitha2006@gmail.com>
2026-01-09 12:56:21 +0530

    Scopes

---
+++ JavaScript_Hands_On/03_Basics/03_Scopes.js
@@ -0,0 +1,25 @@
+//let,var,const
+
+var c = 300;
+const b = 300;
+let a = 300;
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+  console.log("Inner ", a);
+  console.log("Inner ", b);
+  console.log("Inner ", c);
+}
+
+// console.log(a); //here a is 300
+// console.log(b); //here b is 300
+// console.log(c); //here var is 30 not 300
+
+//When ever a var is used it is created in the global scope
+
+const myNewArray = [200, 400, 500, 2000];
+for (let i = 0; i < myNewArray.length; i++) {
+  const element = myNewArray[i];
+  console.log(element);
+}
